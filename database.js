fed13c78150a75dbf2e480e64b52d39f7a8a012b
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-06-21 21:55:42 +0530

    Mongo db

---
+++ database.js
@@ -0,0 +1,3 @@
+const URL = "mongodb+srv://adityasharma:Aditya@4231@namstenodejs.ti67bsd.mongodb.net/"
+
+
--- server.js
+++ server.js
@@ -1,10 +1,35 @@
- const http= require("http")
+//  const http= require("http")
 
- const server = http.createServer(function(req, res){
-  if(req.url === "/data"){
-    res.end("WHAT DID YOU MEAN BY DATA !!!!")
-  }
-  res.end("Hello world !!")
- })
+//  const server = http.createServer(function(req, res){
+//   if(req.url === "/data"){
+//     res.end("WHAT DID YOU MEAN BY DATA !!!!")
+//   }
+//   res.sendFile("/data.json")
+//   res.end("Hello world !!")
+//  })
 
- server.listen(4231)
+//  server.listen(4231)
+// server.js
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  // Set the content type to HTML
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  // Read the HTML file and send it as the response
+  fs.readFile(path.join(__dirname, "data.json"), (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error loading data.json");
+      return;
+    }
+    res.end(data);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
